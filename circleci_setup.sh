c6efe8ea460b2bd0da0246c681c8336fe211fffa
Conor O'Donnell <conor@hubspot.com>
2018-04-25 21:18:25 +0100

    Add release signing

---
+++ circleci_setup.sh
@@ -0,0 +1,6 @@
+touch user.properties
+echo "googleMapsApiKey=$GOOGLE_MAPS_API_KEY" >> user.properties
+echo "storeFile=$BUS_STORE_FILE" >> user.properties
+echo "storePassword=$BUS_STORE_PASSWORD" >> user.properties
+echo "keyAlias=$BUS_KEY_ALIAS" >> user.properties
+echo "keyPassword=$BUS_KEY_PASSWORD" >> user.properties
